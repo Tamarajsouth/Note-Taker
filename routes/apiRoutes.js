b5a96be0dac32b25878520b6cbaea001ec2af1ba
tamara south <tjeansouth@gmail.com>
2020-04-24 16:23:25 -0400

    added some dependencies, restructured files, edit app.get and app.post functions

---
+++ routes/apiRoutes.js
@@ -0,0 +1,61 @@
+// ===============================================================================
+// DEPENDENCIES
+const fs = require("fs");
+const db = require("../db/db.json");
+const express = require("express");
+
+// API GET Requests
+// .get/api/notes
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.send(db);
+  });
+
+  app.post("/api/notes", function (req, res) {
+    // let noteId = uuid();
+    let newNote = {
+      id: noteId,
+      title: req.body.title,
+      text: req.body.text,
+    };
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+
+      const allNotes = JSON.parse(data);
+
+      allNotes.push(newNote);
+
+      fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), (err) => {
+        if (err) throw err;
+        res.send(db);
+        console.log("Note created!");
+      });
+    });
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    let noteId = req.params.id;
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+
+      const allNotes = JSON.parse(data);
+      const newAllNotes = allNotes.filter(note);
+
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(newAllNotes, null, 2),
+        (err) => {
+          if (err) throw err;
+          res.send(db);
+          console.log("Note deleted!");
+        }
+      );
+    });
+  });
+};
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
-var apiRoutes = require("./routes/apiRoutes");
-var htmlRoutes = require("./routes/htmlRoutes");
-var fs = require("fs");
-
+const express = require("express");
+const path = require("path");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));  
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
 // Starts the server to begin listening
 app.listen(PORT, () => console.log("Listening on PORT" + PORT));
