34202ae52dae98920e63efb30f395318f2f70dfe
tamara south <tjeansouth@gmail.com>
2020-04-27 13:42:34 -0400

    fixed problem with syncing of api and html routes and solved problem with index.js - edit consts to vars

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,57 +12,53 @@ const uuid = require("uuid/v4");
 // ROUTING
 // ===============================================================================
 
-  router.get("/notes", function (req, res) {
-    // res.send(db);
-    fs.readFile("./db/db.json", "utf8", err => { 
-      if (err) throw err;
-    })
+router.get("/notes", function (req, res) {
+  // res.send(db);
+  fs.readFile("./db/db.json", "utf8", (err) => {
+    if (err) throw err;
+    res.json(db);
   });
-  // POST
-  router.post("/notes", function (req, res) {
-    // uuid called to generate random id
-    const noteId = uuid();
-    // object array for new note
-    let newNote = {
-      id: noteId,
-      title: req.body.title,
-      text: req.body.text,
-    };
+});
+// POST
+router.post("/notes", function (req, res) {
+  // uuid called to generate random id
+  let noteId = uuid();
+  // object array for new note
+  let newNote = {
+    id: noteId,
+    title: req.body.title,
+    text: req.body.text,
+  };
 
-    // reads JSON file and stores data as variable
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+  // reads JSON file and stores data as variable
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const allNotes = JSON.parse(data);
+    allNotes.push(newNote);
+
+    // adding new data and pushing that data into new note created
+    fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) => {
       if (err) throw err;
-      const allNotes = JSON.parse(data);
-      allNotes.push(newNote);
-
-      // adding new data and pushing that data into new note created
-      fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) => {
-        if (err) throw err;
-        console.log("Note created!");
-        res.send(db);
-      });
+      console.log("Note created!");
+      res.send(db);
     });
   });
-  // delete note function
-  router.delete("/notes/:id", (req, res) => {
-    let noteId = req.params.id;
-    // reads data in JSON file
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-      console.log(data);
-      const allNotes = JSON.parse(data);
-      const newAllNotes = allNotes.ffilter(notes => notes.id !== noteId);
+});
+// delete note function
+router.delete("/notes/:id", (req, res) => {
+  let noteId = req.params.id;
+  // reads data in JSON file - edit data to db **************
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const allNotes = JSON.parse(data);
+    const newAllNotes = allNotes.filter((notes) => notes.id !== noteId);
 
-      fs.writeFile(
-        "./db/db.json",
-        JSON.stringify(newAllNotes),
-        (err) => {
-          if (err) throw err;
-          console.log("Note deleted!");
-          res.send(db);
-        }
-      );
+    fs.writeFile("./db/db.json", JSON.stringify(newAllNotes), (err) => {
+      if (err) throw err;
+      console.log("Note deleted!");
+      res.send(db);
     });
   });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));  
-app.use("/", htmlRoutes);
 app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log("Listening on PORT" + PORT));
